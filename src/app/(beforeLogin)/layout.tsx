8063e1c5f152cf657b8b254a895fdb492ca6e741
jinri yoo <zzznly@gmail.com>
2024-01-05 17:42:39 +0900

    인터셉팅 라우트로 수정

--- src/app/(beforeLogin)/layout.tsx
+++ src/app/(beforeLogin)/layout.tsx
@@ -11,11 +11,19 @@ export default function Layout({ children, modal }: Props) {
   );
 }
 
-/**
+/*
   http://localhost:3000 일때,
-    * children -> page.tsx
-    * modal -> @modal/default.tsx
+    children -> page.tsx
+    modal -> @modal/default.tsx
+
   http://localhost:3000/i/flow/login 일때,
-    * children -> i/flow/login/page.tsx
-    * modal -> @modal/i/flow/login/page.tsx
-**/
+    children -> i/flow/login/page.tsx
+    modal -> @modal/i/flow/login/page.tsx
+
+  여기서, children -> page.tsx, page -> @modal/.../login/page 가 되도록 하려면 Intercepting Routes 사용
+*/
+
+/*
+  children -> /i/.., /login/.., page.tsx
+  modal -> /@modal/..
+*/
